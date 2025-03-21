b6a6c87e6d00a0d780e66ea6bb75d6a5282d6d77
Isma <shadqi1890@gmail.com>
2025-03-22 04:55:20 +0700

    Base

--- database/migrations/2025_03_20_071746_create_pakets_table.php
+++ database/migrations/2025_03_20_071746_create_pakets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama paket');
             $table->string('kategori');
-            $table->int('harga');
+            $table->integer('harga');
             $table->string('kecepatan');
             $table->timestamps();
         });
