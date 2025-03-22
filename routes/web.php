358196cc84dc4d85f956bc13d8f0780f07589518
LegarSuryantara <legarsuryantara@gmail.com>
2025-03-22 12:07:37 +0700

    login-interface

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('listpaket');
 });
+Route::get('login', function () {
+    return view('login');
+});
 
 Route::prefix('admin')->group(function () {
     Route::resource('pakets', adminPaketsController::class);
